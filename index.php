3f15efdd3a3d520feb60567ae64e3964efa8b1f0
ddaaat <sadhat91@gmail.com>
2018-07-03 11:31:25 +0800

    class activity4

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="receiver.php" method="post">
+<input type="text" name="username" placeholder="username"><br>
+<input type="password" name="password" placeholder="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ receiver.php
@@ -0,0 +1,8 @@
+<?php
+$user_name = $_POST['username'];
+$pass_word = $_POST['password'];
+
+echo "Username: " $user_name;
+echo "Password: ""<br>";
+echo $pass_word;
+?>
